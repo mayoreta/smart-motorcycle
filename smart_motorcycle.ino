7f68e5a8d0194d4b0de09ad85e0aaa3adc04adda
Loviana Mayoreta <lovianamayoreta@gmail.com>
2021-10-30 07:38:56 +0700

    remove comment

--- smart_motorcycle.ino
+++ smart_motorcycle.ino
@@ -3,13 +3,17 @@
 #include <Adafruit_PN532.h>
 #include <Adafruit_Fingerprint.h>
 #include <SoftwareSerial.h>
-#include <avr/wdt.h> 
+#include <avr/wdt.h>
 
-#define PN532_IRQ   (2)
+#define PN532_IRQ (2)
 #define PN532_RESET (3)
+#define pinPower (6)
+#define pinStarter (7)
+#define pinAlarm (8)
+#define pinSelenoid (9)
+#define pinAntiTheft (10)
 
 SoftwareSerial fingerSerial(5, 4);
-
 Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&fingerSerial);
 
@@ -20,25 +24,17 @@ int stateAlarm = 0;
 int userId = 0;
 int id = 0;
 
-// pin initiate
-int pinPower = 6;
-int pinStarter = 7;
-int pinAlarm = 8;
-int pinSelenoid = 9;
-int pinAntiTheft = 10;
-
-// id card  
-String userKey = "119372152";
-String userKeyReg = "4931404214992128";
-String userKeyUnReg = "10616117821";
+// id card
+uint8_t userKey[] = {119,37,21,52};
+uint8_t userKeyReg[] = {4,93,140,42,149,92,128};
+uint8_t userKeyUnReg[] = {106,161,178,21};
 
 // millis
 unsigned long previousAlarmMillis = 0;
-unsigned long periodAlarm = 60000;
 unsigned long previousMillis = 0;
-unsigned long period = 10000;
 
 void setup() {
+  wdt_disable();
   fingerSerial.begin(9600);
   Serial.begin(115200);
   while (!Serial) delay(10); // for Leonardo/Micro/Zero
@@ -64,8 +60,9 @@ void loop() {
     break;
     case 1:
       id = finger.templateCount + 1;
-      finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 100, FINGERPRINT_LED_PURPLE);
+      wdt_disable();
       while(!enrollFingerprint());
+      wdt_enable(WDTO_4S);
       stateFingerprint = 0;
     break;
     case 2:
@@ -86,12 +83,12 @@ void loop() {
     logicState();
   }
 
-  if (millis() - previousAlarmMillis >= periodAlarm && stateAlarm == 1) {
+  if (millis() - previousAlarmMillis >= 60000 && stateAlarm == 1) {
     stateAlarm = 0;
     digitalWrite(pinAlarm, HIGH);
   }
 
-  if (millis() - previousMillis >= period && state == 1) {
+  if (millis() - previousMillis >= 10000 && state == 1) {
     state = 2;
   }
 
@@ -120,11 +117,6 @@ void initiateNfc() {
     while (1); // halt
   }
 
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-
   nfc.setPassiveActivationRetries(0xFF);
 
   nfc.SAMConfig();
@@ -144,13 +136,6 @@ void initiateFingerprint() {
 
   Serial.println(F("Reading sensor parameters"));
   finger.getParameters();
-  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
-  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
-  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
-  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
-  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
-  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
-  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
 
   finger.getTemplateCount();
 
@@ -164,29 +149,34 @@ void initiateFingerprint() {
 
 void readNfc() {
   boolean success;
-  String resultId;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
   uint8_t uidLength;
+
+  int matchUserKey = 1;
+  int matchUserKeyReg = 1;
+  int matchUserKeyUnReg = 1;
 
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength, 250);
   
   if (success) {
-    Serial.println("Found a card!");
-    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("UID Value: ");
     for (uint8_t i=0; i < uidLength; i++) 
     {
-      resultId += uid[i];
-      Serial.print(" 0x");Serial.print(uid[i], HEX); 
+      if (uid[i] != userKey[i] && matchUserKey) {
+        matchUserKey = 0;
+      }
+      if (uid[i] != userKeyReg[i] && matchUserKeyReg) {
+        matchUserKeyReg = 0;
+      }
+      if (uid[i] != userKeyUnReg[i] && matchUserKeyUnReg) {
+        matchUserKeyUnReg = 0;
+      }
     }
 
-    Serial.println("");
-
-    if (resultId == userKey) {
+    if (matchUserKey) {
       state += 1;
-    } else if (resultId == userKeyReg) {
+    } else if (matchUserKeyReg) {
       stateFingerprint = 1;
-    } else if (resultId == userKeyUnReg) {
+    } else if (matchUserKeyUnReg) {
       stateFingerprint = 2;
     }
 
@@ -203,13 +193,7 @@ uint8_t readFingerprint() {
       Serial.println("Image taken");
       break;
     case FINGERPRINT_NOFINGER:
-      Serial.println("No finger detected");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
+      Serial.println(".");
       return p;
     default:
       Serial.println("Unknown error");
@@ -223,18 +207,6 @@ uint8_t readFingerprint() {
     case FINGERPRINT_OK:
       Serial.println("Image converted");
       break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
     default:
       Serial.println("Unknown error");
       return p;
@@ -247,9 +219,6 @@ uint8_t readFingerprint() {
     state += 1;
     logicState();
     delay(1000);
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
   } else if (p == FINGERPRINT_NOTFOUND) {
     Serial.println("Did not find a match");
     finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_RED, 8);
@@ -268,6 +237,8 @@ uint8_t readFingerprint() {
 }
 
 uint8_t enrollFingerprint() {
+  finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 100, FINGERPRINT_LED_PURPLE);
+  
   int p = -1;
   Serial.print("Waiting for valid finger to enroll as #"); Serial.println(id);
   while (p != FINGERPRINT_OK) {
@@ -278,12 +249,6 @@ uint8_t enrollFingerprint() {
       break;
     case FINGERPRINT_NOFINGER:
       Serial.println(".");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
       break;
     default:
       Serial.println("Unknown error");
@@ -298,18 +263,6 @@ uint8_t enrollFingerprint() {
     case FINGERPRINT_OK:
       Serial.println("Image converted");
       break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
     default:
       Serial.println("Unknown error");
       return p;
@@ -322,7 +275,7 @@ uint8_t enrollFingerprint() {
   while (p != FINGERPRINT_NOFINGER) {
     p = finger.getImage();
   }
-  Serial.print("ID "); Serial.println(id);
+
   p = -1;
   Serial.println("Place same finger again");
   finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 100, FINGERPRINT_LED_PURPLE);
@@ -333,13 +286,7 @@ uint8_t enrollFingerprint() {
       Serial.println("Image taken");
       break;
     case FINGERPRINT_NOFINGER:
-      Serial.print(".");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
+      Serial.println(".");
       break;
     default:
       Serial.println("Unknown error");
@@ -354,18 +301,6 @@ uint8_t enrollFingerprint() {
     case FINGERPRINT_OK:
       Serial.println("Image converted");
       break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
     default:
       Serial.println("Unknown error");
       return p;
@@ -377,12 +312,6 @@ uint8_t enrollFingerprint() {
   p = finger.createModel();
   if (p == FINGERPRINT_OK) {
     Serial.println("Prints matched!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
-    Serial.println("Fingerprints did not match");
-    return p;
   } else {
     Serial.println("Unknown error");
     return p;
@@ -392,25 +321,15 @@ uint8_t enrollFingerprint() {
   p = finger.storeModel(id);
   if (p == FINGERPRINT_OK) {
     Serial.println("Stored!");
-    finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_ON, 200, FINGERPRINT_LED_BLUE);
-    delay(2000);
-    finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 200, FINGERPRINT_LED_BLUE);
-    delay(2000);
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_BADLOCATION) {
-    Serial.println("Could not store in that location");
-    return p;
-  } else if (p == FINGERPRINT_FLASHERR) {
-    Serial.println("Error writing to flash");
-    return p;
   } else {
     Serial.println("Unknown error");
     return p;
   }
 
-  return true;
+  finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 100, FINGERPRINT_LED_BLUE, 3);
+  delay(2000);
+
+  return true;  
 }
 
 void logicState() {
